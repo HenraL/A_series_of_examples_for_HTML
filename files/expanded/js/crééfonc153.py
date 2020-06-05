e8f543bb16a1156bd039a886df53a7bfab844a2e
HenraL <37299738+HenraL@users.noreply.github.com>
2020-06-05 10:51:08 +0200

    created the expanded version of the html page

---
+++ files/expanded/js/crééfonc153.py
@@ -0,0 +1,13 @@
+g=2
+count=2
+while g<=153:
+    print("                document.getElementById('baliseColorFin{}').innerHTML=colChoice;".format(count))
+    g+=1
+    count+=1
+print()
+g=2
+count=2
+while g<=153:
+    print("                document.getElementById('semiFin{}').innerHTML=semiiChoice;".format(count))
+    g+=1
+    count+=1
